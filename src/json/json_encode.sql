57528dfd5d5c8c75d0410d50894f7165db36f602
Stephan Plepelits <skunk@xover.mud.at>
2012-05-17 07:30:00 +0200

    json_encode: Handle more data types: bigint, float, text[], bigint[], float[]

--- src/json/json_encode.sql
+++ src/json/json_encode.sql
@@ -1,21 +1,3 @@
-CREATE OR REPLACE FUNCTION json_encode(hstore)
-RETURNS text
-AS $$
-DECLARE
-  value	ALIAS FOR $1;
-  entry	record;
-  list	text[]=Array[]::text[];
-BEGIN
-  for entry in select * from each(value) loop
--- add to list of prepared return strings
-    list:=array_append(list, json_encode(entry.key)||':'||json_encode(entry.value));
-  end loop;
-
--- write {} around values and separate by ,
-  return '{'||array_to_string(list, ',')||'}';
-END
-$$ LANGUAGE 'plpgsql';
-
 CREATE OR REPLACE FUNCTION json_encode(text)
 RETURNS text
 AS $$
@@ -36,5 +18,53 @@ BEGIN
 
 -- Enclose in ", return
   return '"'||ret||'"';
+END
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION json_encode(bigint)
+RETURNS text
+AS $$
+  select cast($1 as text);
+$$ LANGUAGE 'sql';
+
+CREATE OR REPLACE FUNCTION json_encode(float)
+RETURNS text
+AS $$
+  select cast($1 as text);
+$$ LANGUAGE 'sql';
+
+CREATE OR REPLACE FUNCTION json_encode(hstore)
+RETURNS text
+AS $$
+DECLARE
+  value	ALIAS FOR $1;
+  entry	record;
+  list	text[]=Array[]::text[];
+BEGIN
+  for entry in select * from each(value) loop
+-- add to list of prepared return strings
+    list:=array_append(list, json_encode(entry.key)||':'||json_encode(entry.value));
+  end loop;
+
+-- write {} around values and separate by ,
+  return '{'||array_to_string(list, ',')||'}';
+END
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION json_encode(anyarray)
+RETURNS text
+AS $$
+DECLARE
+  value	ALIAS FOR $1;
+  entry	record;
+  list	text[]=Array[]::text[];
+BEGIN
+  for entry in select unnest as value from unnest(value) loop
+-- add to list of prepared return strings
+    list:=array_append(list, json_encode(entry.value));
+  end loop;
+
+-- write {} around values and separate by ,
+  return '['||array_to_string(list, ',')||']';
 END
 $$ LANGUAGE 'plpgsql';
