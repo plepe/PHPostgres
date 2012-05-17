59a39e4af907aef5a8a41d71d80d92c0d52f52b9
Stephan Plepelits <skunk@xover.mud.at>
2012-05-17 07:15:03 +0200

    json_encode: Also encode text values

--- src/json/json_encode.sql
+++ src/json/json_encode.sql
@@ -7,27 +7,34 @@ DECLARE
   list	text[]=Array[]::text[];
 BEGIN
   for entry in select * from each(value) loop
--- Quote characters
-    entry.key=replace(entry.key, E'\\', E'\\\\');
-    entry.key=replace(entry.key, E'"', E'\\"');
-    entry.key=replace(entry.key, E'\b', E'\\b');
-    entry.key=replace(entry.key, E'\t', E'\\t');
-    entry.key=replace(entry.key, E'\n', E'\\n');
-    entry.key=replace(entry.key, E'\f', E'\\f');
-    entry.key=replace(entry.key, E'\r', E'\\r');
-    entry.value=replace(entry.value, E'\\', E'\\\\');
-    entry.value=replace(entry.value, E'"', E'\\"');
-    entry.value=replace(entry.value, E'\b', E'\\b');
-    entry.value=replace(entry.value, E'\t', E'\\t');
-    entry.value=replace(entry.value, E'\n', E'\\n');
-    entry.value=replace(entry.value, E'\f', E'\\f');
-    entry.value=replace(entry.value, E'\r', E'\\r');
-
 -- add to list of prepared return strings
-    list:=array_append(list, '"'||entry.key||'":"'||entry.value||'"');
+    list:=array_append(list, json_encode(entry.key)||':'||json_encode(entry.value));
   end loop;
 
 -- write {} around values and separate by ,
   return '{'||array_to_string(list, ',')||'}';
+END
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION json_encode(text)
+RETURNS text
+AS $$
+DECLARE
+  value	ALIAS FOR $1;
+  ret text;
+BEGIN
+  ret:=value;
+
+-- Quote characters
+  ret=replace(ret, E'\\', E'\\\\');
+  ret=replace(ret, E'"', E'\\"');
+  ret=replace(ret, E'\b', E'\\b');
+  ret=replace(ret, E'\t', E'\\t');
+  ret=replace(ret, E'\n', E'\\n');
+  ret=replace(ret, E'\f', E'\\f');
+  ret=replace(ret, E'\r', E'\\r');
+
+-- Enclose in ", return
+  return '"'||ret||'"';
 END
 $$ LANGUAGE 'plpgsql';
